b8496c9bb1c5e942b36e28e0a3612bb32e033135
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-10-02 10:11:40 +0200

    Adss merge methods and data

--- tst.js
+++ tst.js
@@ -2,6 +2,8 @@
 
 var preparePoliticians = require('./lib/preparePoliticians')
 var updateItems = require('./lib/updateItems')
+var unwrapPoliticians = require('./lib/unwrapPoliticians')
+var writeToFile = require('./lib/writeToFile')
 var politicians = require('./data/politicians.json')
 var committees = require('./data/politiciansCommittee.json')
 var indexedObject = preparePoliticians({
@@ -14,6 +16,16 @@ var updatedObject = updateItems({
   indexedObject: indexedObject
 })
 
-console.log(updatedObject)
+var unwrappedList = unwrapPoliticians(updatedObject)
+
+console.log(unwrappedList)
+
+writeToFile('mergedData', JSON.stringify(unwrappedList), function (err, data) {
+  if (err) {
+    console.error(err)
+  } else {
+    console.log(data)
+  }
+})
 
 console.log('Finished')
