974c2d3332f2eeec43f90c5f14a79cde9e53d050
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-10-20 07:24:26 +0200

    Adds test

---
+++ test/index-test.js
@@ -0,0 +1,38 @@
+'use strict'
+
+var tap = require('tap')
+var nep = require('./index')
+
+tap.test('Options object must be supplied', function (test) {
+  var options = false
+  var expectedErrorMessage = 'Missing required input: options object'
+  nep(options, function errorIfMissingOptions (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
+  })
+})
+
+tap.test('Options.storage must be supplied', function (test) {
+  var options = {
+    storage: false
+  }
+  var expectedErrorMessage = 'Missing required input: options.storage'
+  nep(options, function errorIfMissingFiles (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
+  })
+})
+
+tap.test('Options.p360', function (test) {
+  var options = {
+    storage: {
+      path: 'data'
+    },
+    p360: false
+  }
+  var expectedErrorMessage = 'Missing required input: options.p360'
+  nep(options, function errorIfMissingStoragePath (error, data) {
+    tap.equal(error.message, expectedErrorMessage, expectedErrorMessage)
+    test.done()
+  })
+})
